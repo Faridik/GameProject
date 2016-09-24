fe9c720595fd6e8decd12495db4f344238572cd2
Sergey Golubev <ser9800@icloud.com>
2016-09-24 22:00:56 +0300

    mac os and windows

--- GameProject/Project.h
+++ GameProject/Project.h
@@ -2,11 +2,15 @@
 #define _PROJECT_H_
 
 #pragma comment(lib,"SDL2.lib")
-
+#ifdef _WIN32
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_ttf.h>
-
+#elif defined __APPLE__
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include <SDL2_ttf/SDL_ttf.h>
+#endif
 #include "Game.h"
 #include "Graphics.h"
 #include "Image.h"
--- GameProject/main.cpp
+++ GameProject/main.cpp
@@ -1,7 +1,7 @@
 #include "Project.h"
 
-int SDL_main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	Game game;
-	return game.Execute(new Game_Play(), "GameProject", 800, 600);
+    Game game;
+    return game.Execute(new Game_Play(), "GameProject", 800, 600);
 }
